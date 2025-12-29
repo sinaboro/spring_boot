8bb58646af583d29a5d662d775b9cccb0344c990
kimdaechul <rlaeocjf@gmail.com>
2025-12-29 18:00:18 +0900

    message

--- demo/src/main/java/com/example/demo/controller/MemberController.java
+++ demo/src/main/java/com/example/demo/controller/MemberController.java
@@ -1,4 +1,27 @@
 package com.example.demo.controller;
 
+import com.example.demo.domain.MemberDTO;
+import com.example.demo.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/member")
+@Log4j2
+@RequiredArgsConstructor
 public class MemberController {
+    private final MemberService memberService;
+
+    @GetMapping("/list")
+    public void list(Model model){
+        log.info("List...........");
+        List<MemberDTO> getList = memberService.getList();
+        model.addAttribute("list", getList);
+    }
 }
--- demo/src/main/java/com/example/demo/service/MemberService.java
+++ demo/src/main/java/com/example/demo/service/MemberService.java
@@ -1,4 +1,22 @@
 package com.example.demo.service;
 
+import com.example.demo.domain.MemberDTO;
+import com.example.demo.mapper.MemberMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Log4j2
+@RequiredArgsConstructor
 public class MemberService {
+
+    private final MemberMapper memberMapper;
+
+    public List<MemberDTO> getList(){
+        return memberMapper.findAll();
+    }
+
 }
