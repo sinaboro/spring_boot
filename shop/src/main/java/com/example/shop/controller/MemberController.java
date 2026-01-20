8c39bd533b6527f90585a75c0d8eb1fe76d47809
kimdaechul <rlaeocjf@gmail.com>
2026-01-20 12:46:48 +0900

    message

--- shop/src/main/java/com/example/shop/controller/MemberController.java
+++ shop/src/main/java/com/example/shop/controller/MemberController.java
@@ -3,11 +3,13 @@ package com.example.shop.controller;
 import com.example.shop.dto.MemberFormDto;
 import com.example.shop.entity.Member;
 import com.example.shop.service.MemberService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,10 +31,34 @@ public class MemberController {
     }
 
     @PostMapping(value = "/new")
-    public String MemberForm(MemberFormDto memberFormDto){
-        Member member = Member.createMember(memberFormDto, passwordEncoder);
-        memberService.saveMember(member);
+    public String MemberForm(@Valid MemberFormDto memberFormDto,
+                             BindingResult bindingResult, Model model){
+
+        if(bindingResult.hasErrors()){
+            return "member/memberForm";
+        }
+
+        try {
+            Member member = Member.createMember(memberFormDto, passwordEncoder);
+            memberService.saveMember(member);
+        }catch(IllegalStateException e){
+            model.addAttribute("errorMessage", e.getMessage());
+            return "member/memberForm";
+        }
 
         return "redirect:/";
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
--- shop/src/main/java/com/example/shop/dto/MemberFormDto.java
+++ shop/src/main/java/com/example/shop/dto/MemberFormDto.java
@@ -1,6 +1,10 @@
 package com.example.shop.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.*;
+import org.hibernate.validator.constraints.Length;
 
 @Getter
 @Setter
@@ -9,8 +13,20 @@ import lombok.*;
 @NoArgsConstructor
 public class MemberFormDto {
 
+    //공백도 허용하지 않겠다.
+    @NotBlank(message = "이름은 필수 입력 값입니다.")
     private String name;
+
+    //공백문자열 허용
+    @NotEmpty(message = "이메일은 필수 입력 값입니다.")
+    @Email(message = "이메일 형식으로 입력해주세요.")
     private String email;  //회원 가입시 아이디로 사용~
+
+    @NotEmpty(message = "비밀번호는 필수 입력 값입니다.")
+    @Length(min = 4, max = 16, message = "비밀번호는 4자 이상, 16 이하로 입력해주세요")
     private String password;
+
+    //""
+    @NotEmpty(message = "주소는 필수 입력 값입니다.")
     private String address;
 }
