35c37aaebf8136b61706d9a38dcfbcb7f32a673c
kimdaechul <rlaeocjf@gmail.com>
2026-01-20 09:24:54 +0900

    message

---
+++ shop/src/main/java/com/example/shop/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.example.shop.config;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@Log4j2
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http){
+
+        log.info("---------------securityFilterChain---------------------------");
+        return null;
+    }
+}
