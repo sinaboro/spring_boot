7b4ad64b4be1ad0b522a851d08afd65eb162a35b
kimdaechul <rlaeocjf@gmail.com>
2026-01-20 15:13:57 +0900

    message

--- shop/src/main/java/com/example/shop/config/SecurityConfig.java
+++ shop/src/main/java/com/example/shop/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.util.AntPathMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -16,8 +17,24 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http){
-
         log.info("---------------securityFilterChain---------------------------");
+
+        http
+                .formLogin(login -> login
+                        .loginPage("/members/login")
+                        .loginProcessingUrl("/members/login")
+                        .failureUrl("/members/login/error")
+                        .defaultSuccessUrl("/", true)
+                        .usernameParameter("email") //name: username 기입할 필요 없음
+                        //.passwordParameter("pwd") //만약 name: pasword아니고 name: pwd
+                )
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/members/logout")
+                        .logoutSuccessUrl("/")
+                );
+
+
+
         return http.build();
     }
 
--- shop/src/main/java/com/example/shop/service/MemberService.java
+++ shop/src/main/java/com/example/shop/service/MemberService.java
@@ -5,6 +5,10 @@ import com.example.shop.entity.Member;
 import com.example.shop.repository.MemberRepoistory;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,7 +19,7 @@ import java.util.Optional;
 @Log4j2
 @AllArgsConstructor
 @Transactional
-public class MemberService {
+public class MemberService implements UserDetailsService {
 
     private final MemberRepoistory memberRepoistory;
 
@@ -26,5 +30,19 @@ public class MemberService {
             throw new IllegalStateException("이미 가입된 회원입니다.");
 
         return memberRepoistory.save(member);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        log.info("------------email------------------ : " + email);
+
+        Member member = memberRepoistory.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(email + "해당하는 회원이 없습니다."));
+
+        return User.builder()
+                .username(member.getName())
+                .password(member.getPassword())
+                .roles(member.getRole().toString())
+                .build();
     }
 }
