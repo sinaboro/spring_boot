954524030c50e1ae2c6decc82e910fb88e99f9ed
kimdaechul <rlaeocjf@gmail.com>
2026-01-23 11:06:29 +0900

    message

--- shop/src/main/java/com/example/shop/controller/ItemController.java
+++ shop/src/main/java/com/example/shop/controller/ItemController.java
@@ -1,22 +1,58 @@
 package com.example.shop.controller;
 
 import com.example.shop.dto.ItemFormDto;
+import com.example.shop.dto.ItemImgDto;
+import com.example.shop.service.ItemService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 @Log4j2
 public class ItemController {
 
+    private final ItemService itemService;
+
     @GetMapping(value = "/admin/item/new")
     public String itemForm(Model model){
         model.addAttribute("itemFormDto", new ItemFormDto());
         return "item/itemForm";
+    }
+
+    @PostMapping(value = "/admin/item/new")
+    public String itemForm(@Valid ItemFormDto itemFormDto,
+                           BindingResult bindingResult, Model model,
+                           @RequestParam("itemImgFile") List<MultipartFile> itemImgFileList){
+
+        if(bindingResult.hasErrors()){
+            return "item/itemForm";
+        }
+
+        if(itemImgFileList.get(0).isEmpty() && itemFormDto.getId() == null){
+            model.addAttribute("errorMessage", "첫번째 상품 이미지는 필수 입력 값 입니다.");
+            return "item/itemForm";
+        }
+
+        log.info("itemFormDto.getId() : " + itemFormDto.getId());
+
+        try{
+            itemService.savedItem(itemFormDto, itemImgFileList);
+        }catch (Exception e){
+            model.addAttribute("errorMessage", "상품 등록 중 에러가 발생하였습니다.");
+            return "item/itemForm";
+        }
+        return "redirect:/";
     }
 
 }
