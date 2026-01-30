532b75057f9be59d6f51084a876e4be44c2069c8
kimdaechul <rlaeocjf@gmail.com>
2026-01-30 18:53:06 +0900

    message

---
+++ shop/src/main/java/com/example/shop/repository/CartItemRepository.java
@@ -0,0 +1,12 @@
+package com.example.shop.repository;
+
+
+import com.example.shop.entity.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+
+    CartItem  findByCartIdAndItemId(Long cartId, Long itemId);
+}
---
+++ shop/src/main/java/com/example/shop/service/CartService.java
@@ -0,0 +1,82 @@
+package com.example.shop.service;
+
+import com.example.shop.dto.CartItemDto;
+import com.example.shop.entity.Cart;
+import com.example.shop.entity.CartItem;
+import com.example.shop.entity.Item;
+import com.example.shop.entity.Member;
+import com.example.shop.repository.CartItemRepository;
+import com.example.shop.repository.CartRepository;
+import com.example.shop.repository.ItemRepository;
+import com.example.shop.repository.MemberRepository;
+import jakarta.persistence.EntityExistsException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@Log
+@RequiredArgsConstructor
+@Log4j2
+@Transactional
+public class CartService {
+
+    private final ItemRepository itemRepository;
+    private final MemberRepository memberRepository;
+    private final CartRepository cartRepository;
+    private final CartItemRepository cartItemRepository;
+
+
+    public Long addCart(CartItemDto cartItemDto, String email){
+        Item item = itemRepository.findById(cartItemDto.getItemId())
+                .orElseThrow(EntityExistsException::new);
+
+        Member member = memberRepository.findByEmail(email)
+                .orElseThrow(EntityExistsException::new);
+
+        Cart cart = cartRepository.findByMemberId(member.getId());
+
+        if(cart == null) {
+            cart = Cart.createCart(member);
+            cartRepository.save(cart);
+        }
+
+        CartItem savedCartItem =
+                cartItemRepository.findByCartIdAndItemId(cart.getId(), item.getId());
+
+        // 장바구니 구매하고자 하는 상품이 존재한다면
+        if(savedCartItem != null){
+            savedCartItem.addCount(cartItemDto.getCount());
+            return  savedCartItem.getId();
+        }else{
+            CartItem cartItem = CartItem.createCartItem(cart, item, cartItemDto.getCount());
+            cartItemRepository.save(cartItem);
+            return cartItem.getId();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
